14fc70a46efbcc0051498ae2b4f0d4393c14106c
daiki98 <zdymori@gmail.com>
2021-08-19 17:05:42 +0900

    memo app

--- basic_calculation/MainPage.xaml.cs
+++ basic_calculation/MainPage.xaml.cs
@@ -17,3 +17,57 @@ namespace basic_calculation
         }
     }
 }
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Xamarin.Forms;
+
+//namespace XAMLUI.ViewModels
+//{
+//    public class MainPage : INotifyPropertyChanged
+//    {
+//        public MainPage()
+//        {
+//            EraseCommand = new Command(() =>
+//            {
+//                TheNote = string.Empty;
+//            });
+
+//            SaveCommand = new Command(() =>
+//            {
+//                AllNotes.Add(TheNote);
+
+//                TheNote = string.Empty;
+//            });
+
+
+
+//        }
+
+//        public ObservableCollection<string> AllNotes { get; set; }
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+
+//        string theNote;
+
+//        public string TheNote
+//        {
+//            get => theNote;
+//            set
+//            {
+//                theNote = value;
+
+//                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+//                PropertyChanged?.Invoke(this, args);
+//            }
+//        }
+
+//        public Command SaveCommand { get; }
+//        public Command EraseCommand { get; }
+//    }
+//}
