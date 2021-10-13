ce3ae6f10b504c9ce8327f9086a1aa021a6be9a3
Dsuke528 <dsuke7185@icloud.com>
2021-10-13 16:06:27 +0900

    総当たり計算入れた

--- basic_calculation/basic_calculation/Calculate.cs
+++ basic_calculation/basic_calculation/Calculate.cs
@@ -938,6 +938,94 @@ namespace basic_calculation
 
             return res;
         }
+
+        //後置記法　→　計算
+        public static string Calculation(string input)
+        {
+            Stack<decimal> calcResult = new Stack<decimal>();
+            string space = " ";
+            char Space = space[0];
+            string res = null;
+            decimal m = -100.00M;//代入値の初期値
+
+            for (double num = -100.00D; num <= 2000.00D; num += 0.01)//代入値(n)
+            {
+                string res2 = input.Replace("□", m.ToString());
+                string[] res3 = res2.Trim().Split(Space);
+                foreach (string token in res3)
+                {
+                    switch (token)
+                    {
+                        case "+":
+                            decimal A0 = calcResult.Pop();
+                            decimal B0 = calcResult.Pop();
+                            calcResult.Push(B0 + A0);
+                            break;
+
+                        case "-":
+                            decimal A1 = calcResult.Pop();
+                            decimal B1 = 0;
+                            if (calcResult.Count > 0)
+                            {
+                                B1 = calcResult.Pop();
+                            }
+                            calcResult.Push(B1 - A1);
+                            break;
+
+                        case "*":
+                            decimal A2 = calcResult.Pop();
+                            decimal B2 = calcResult.Pop();
+                            decimal AB2 = decimal.Round(B2 * A2, 5);
+                            calcResult.Push(AB2);
+                            break;
+
+                        case "/":
+                            decimal A3 = calcResult.Pop();
+                            decimal B3 = calcResult.Pop();
+                            if (A3 == 0)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                calcResult.Push(B3 / A3);
+                                //decimal ans = B3 / A3;
+                                //string ans2 = ans.ToString("F8");
+                                //calcResult.Push(decimal.Parse(ans2));
+                                break;
+                            }
+
+                        case "%":
+                            decimal A4 = calcResult.Pop();
+                            decimal B4 = 100;
+                            calcResult.Push(A4 / B4);
+                            break;
+
+                        default:
+                            calcResult.Push(decimal.Parse(token));
+                            break;
+                    }
+                }
+
+                if (calcResult.Peek() == 0)
+                {
+                    res = m.ToString("G29");
+                    break;
+                }
+
+                m += 0.01M;
+
+            }
+
+            //解が範囲外のとき
+            if (res == null)
+            {
+                res = "Out of Range";
+            }
+
+            return res;
+        }
+
     }
 
 }
