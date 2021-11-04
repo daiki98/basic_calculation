d6bba2d2e4c297c14488dbb9c180ea3703f5231b
h59y59 <h59y59@gmail.com>
2021-11-04 22:45:42 +0900

    説明ページつくったよ

--- basic_calculation/basic_calculation/StartPage.xaml.cs
+++ basic_calculation/basic_calculation/StartPage.xaml.cs
@@ -16,9 +16,16 @@ namespace basic_calculation
         {
             InitializeComponent();
             NextButton.Clicked += NextButton_Clicked;
+            ExplainPage.Clicked += ExplainPage_Clicked;
+
             NavigationPage.SetHasNavigationBar(this, false);
 
           //  Direction.Source = ImageSource.FromResource("basic_calculation.Image.DirectionImage.jpg"); 
+        }
+
+        private void ExplainPage_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Explain());
         }
 
         private void NextButton_Clicked(object sender, EventArgs e)
